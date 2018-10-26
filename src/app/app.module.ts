79a1d0dba2979778adb54043832a5493a28fbdad
unknown <luis@ACARBALLEIRA.LOCAL>
2018-10-26 13:45:30 +0200

    Con todas las paginas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,7 @@
+import { TabsPage } from './../pages/tabs/tabs';
+import { ModalPage } from './../pages/modal/modal';
+import { Ajustes2Page } from './../pages/ajustes2/ajustes2';
+import { AjustesPage } from './../pages/ajustes/ajustes';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
@@ -5,12 +9,20 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { PrincipalPage } from '../pages/principal/principal';
+import { Principal2Page } from '../pages/principal2/principal2';
+import { Pagina3Page } from '../pages/pagina3/pagina3';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    PrincipalPage,
+    Principal2Page,
+    Pagina3Page,    
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    TabsPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +31,13 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    PrincipalPage,
+    Principal2Page,
+    Pagina3Page,    
+    AjustesPage,
+    Ajustes2Page,
+    ModalPage,
+    TabsPage
   ],
   providers: [
     StatusBar,
---
+++ src/pages/principal2/principal2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { Principal2Page } from './principal2';
+
+@NgModule({
+  declarations: [
+    Principal2Page,
+  ],
+  imports: [
+    IonicPageModule.forChild(Principal2Page),
+  ],
+})
+export class Principal2PageModule {}
